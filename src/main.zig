7627746d9467c886181e32b645d1f9c73cfd8761
cynumini <cynumini@proton.me>
2025-03-23 21:59:02 +0200

    draw triangle

--- src/main.zig
+++ src/main.zig
@@ -16,6 +16,12 @@ fn processInput(window: *c.GLFWwindow) void {
     }
 }
 
+const screenWidth = 800;
+const screenHeight = 600;
+
+const vertexShaderSource = @embedFile("basic.vert");
+const fragmentShaderSource = @embedFile("basic.frag");
+
 pub fn main() !void {
     _ = c.glfwInit();
     defer c.glfwTerminate();
@@ -24,22 +30,90 @@ pub fn main() !void {
     c.glfwWindowHint(c.GLFW_CONTEXT_VERSION_MINOR, 3);
     c.glfwWindowHint(c.GLFW_OPENGL_PROFILE, c.GLFW_OPENGL_CORE_PROFILE);
 
-    const window = c.glfwCreateWindow(800, 600, "LearnOpenGL", null, null) orelse {
+    const window = c.glfwCreateWindow(screenWidth, screenHeight, "LearnOpenGL", null, null) orelse {
         std.debug.print("Failed to create GLFW window\n", .{});
         c.glfwTerminate();
         return error.CantCreateGLFWWindow;
     };
 
-    _ = c.glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
-
     c.glfwMakeContextCurrent(window);
+    _ = c.glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
 
     if (c.gladLoadGLLoader(@ptrCast(&c.glfwGetProcAddress)) == 0) {
         std.debug.print("Failed to initialize GLAD\n", .{});
         return error.CantInitGLAD;
     }
 
-    c.glViewport(0, 0, 800, 600);
+    // Compile vertex shader
+    const vertexShader = c.glCreateShader(c.GL_VERTEX_SHADER);
+    c.glShaderSource(vertexShader, 1, @ptrCast(&vertexShaderSource), null);
+    c.glCompileShader(vertexShader);
+
+    var success: i32 = undefined;
+    var infoLog: [512]u8 = undefined;
+    c.glGetShaderiv(vertexShader, c.GL_COMPILE_STATUS, &success);
+    if (success == 0) {
+        c.glGetShaderInfoLog(vertexShader, 512, null, &infoLog);
+        std.debug.print("{s}", .{infoLog});
+        return error.ShaderVertexCompilationFailed;
+    }
+
+    // Compile fragment shader
+    const fragmentShader = c.glCreateShader(c.GL_FRAGMENT_SHADER);
+    c.glShaderSource(fragmentShader, 1, @ptrCast(&fragmentShaderSource), null);
+    c.glCompileShader(fragmentShader);
+
+    c.glGetShaderiv(fragmentShader, c.GL_COMPILE_STATUS, &success);
+    if (success == 0) {
+        c.glGetShaderInfoLog(fragmentShader, 512, null, &infoLog);
+        std.debug.print("{s}", .{infoLog});
+        return error.ShaderFragmentCompilationFailed;
+    }
+
+    const shaderProgram = c.glCreateProgram();
+    defer c.glDeleteProgram(shaderProgram);
+
+    c.glAttachShader(shaderProgram, vertexShader);
+    c.glAttachShader(shaderProgram, fragmentShader);
+    c.glLinkProgram(shaderProgram);
+
+    c.glGetProgramiv(shaderProgram, c.GL_LINK_STATUS, &success);
+    if (success == 0) {
+        c.glGetProgramInfoLog(shaderProgram, 512, null, &infoLog);
+        std.debug.print("{s}", .{infoLog});
+        return error.shaderProgramLinkError;
+    }
+
+    c.glDeleteShader(vertexShader);
+    c.glDeleteShader(fragmentShader);
+
+    const vertices = [_]f32{
+        -0.5, -0.5, 0.0, // left
+        0.5, -0.5, 0.0, // right
+        0.0, 0.5, 0.0, // top
+    };
+
+    var VBO: u32, var VAO: u32 = .{ undefined, undefined };
+
+    c.glGenVertexArrays(1, @ptrCast(&VAO));
+    defer c.glDeleteVertexArrays(1, @ptrCast(&VAO));
+
+    c.glGenBuffers(1, @ptrCast(&VBO));
+    defer c.glDeleteBuffers(1, @ptrCast(&VBO));
+
+    // Setup or VAO
+    {
+        c.glBindVertexArray(VAO);
+        defer c.glBindVertexArray(0); // unselect VAO
+
+        c.glBindBuffer(c.GL_ARRAY_BUFFER, VBO);
+        defer c.glBindBuffer(c.GL_ARRAY_BUFFER, 0); // unselect VBO
+
+        c.glBufferData(c.GL_ARRAY_BUFFER, @sizeOf(f32) * vertices.len, &vertices, c.GL_STATIC_DRAW);
+
+        c.glVertexAttribPointer(0, 3, c.GL_FLOAT, c.GL_FALSE, 3 * @sizeOf(f32), @ptrFromInt(0));
+        c.glEnableVertexAttribArray(0);
+    }
 
     // render loop
     while (c.glfwWindowShouldClose(window) != 1) {
@@ -49,6 +123,10 @@ pub fn main() !void {
         // rendering commands here
         c.glClearColor(0.2, 0.3, 0.3, 1.0);
         c.glClear(c.GL_COLOR_BUFFER_BIT);
+
+        c.glUseProgram(shaderProgram);
+        c.glBindVertexArray(VAO);
+        c.glDrawArrays(c.GL_TRIANGLES, 0, 3);
 
         // check and call events and swap the buffers
         c.glfwSwapBuffers(window);
