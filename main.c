c4db69b1f45f16c4e23b4ec35427217042fd4bac
cynumini <cynumini@proton.me>
2026-07-20 13:59:36 +0300

    do "1.2.hello_window_clear"

--- main.c
+++ main.c
@@ -60,6 +60,11 @@ int main(void) {
         // -----
         process_input(window);
 
+        // render
+        // ------
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
         // glfw: swap buffers and poll IO events (keys
         // pressed/released, mouse moved etc.)
         // -------------------------------------------
